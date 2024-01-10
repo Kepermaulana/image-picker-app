7b7107dcc64411565af9e25b3ca76820d37a89a9
Rifqi Maulana <kepermaulana@gmail.com>
2024-01-10 14:46:20 +0700

    first init

---
+++ lib/home_screen.dart
@@ -0,0 +1,115 @@
+import 'dart:io';
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  File? _image;
+  final picker = ImagePicker();
+
+  Future getImageFromGallery() async {
+    final pickedFile =
+        await picker.pickImage(source: ImageSource.gallery, imageQuality: 5);
+
+    final bytes = (await _image!.readAsBytes()).lengthInBytes;
+    final kb = bytes / 1024;
+    final mb = kb / 1024;
+
+    setState(
+      () {
+        if (pickedFile != null) {
+          _image = File(pickedFile.path);
+        }
+        print(_image);
+        print(kb);
+      },
+    );
+  }
+
+  Future getImageFromCamera() async {
+    final pickedFile =
+        await picker.pickImage(source: ImageSource.camera, imageQuality: 5);
+
+    final bytes = (await _image!.readAsBytes()).lengthInBytes;
+    final kb = bytes / 1024;
+    final mb = kb / 1024;
+
+    setState(
+      () {
+        if (pickedFile != null) {
+          _image = File(pickedFile.path);
+        }
+        print(_image);
+        print(kb);
+      },
+    );
+  }
+
+  Future showOptions() async {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (context) => CupertinoActionSheet(
+        actions: [
+          CupertinoActionSheetAction(
+            child: Text('Photo Gallery'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              getImageFromGallery();
+            },
+          ),
+          CupertinoActionSheetAction(
+            child: Text('Camera'),
+            onPressed: () {
+              Navigator.of(context).pop();
+              getImageFromCamera();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Picker'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextButton(
+              child: Text('Pilih Gambar'),
+              onPressed: showOptions,
+            ),
+            Center(
+              child: _image == null
+                  ? Text('Tidak ada gambar yang dipilih')
+                  : Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.file(
+                              File(_image!.path),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
